490e92ed54ee844f38877dea3d143f1798ca0990
Hammersen <thomas.hammersen@gmx.de>
2020-04-14 18:21:51 +0200

    Klasse Sportler hinzugefuegt

---
+++ Sportler.java
@@ -0,0 +1,21 @@
+public class Sportler
+{
+	private String zName;
+	private String zVorname;
+	
+	public Sportler
+	{
+		zName = pName;
+		zVorname = pVorname;
+	}
+	
+	public String gibName()
+	{
+		return zName;
+	}
+	
+	public String gibVorname()
+	{
+		return zVorname;
+	}
+}
