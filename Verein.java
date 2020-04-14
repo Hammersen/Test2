d50b3d4266046566e497d4502aeac5fbbd75c8f7
Hammersen <thomas.hammersen@gmx.de>
2020-04-14 18:09:25 +0200

    Dateiendung auf .java gesetzt

---
+++ Verein.java
@@ -0,0 +1,11 @@
+public class Verein
+{
+  private String zName;
+  private int zGruedungsjahr;
+  
+  public Verein(String pName)
+  {
+    zName = pName;
+    zGruendungsjahr = pGruendungsjahr;
+  }
+}
